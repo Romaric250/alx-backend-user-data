cc356bd3a696157c1fb648545c6919a1cdbd3750
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-13 10:51:04 +0100

    made some changes to the setup

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.route("/about", methods=['GET'])
+def Get():
+    return "now we are talking like adults"
+
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -24,4 +29,4 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
